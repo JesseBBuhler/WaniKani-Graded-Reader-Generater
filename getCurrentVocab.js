08e16e901e45f6bb21c90b25955a9df954461e6a
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-03-23 14:11:07 -0600

    get my current vocab

--- getCurrentVocab.js
+++ getCurrentVocab.js
@@ -1,21 +1,53 @@
 const fs = require("fs");
+const allVocab = require("./vocabData.js");
 
 var apiToken = "b091ea2c-fd95-4d02-a07e-aab83ad14726";
 var apiEndpointPath = "assignments";
 var requestHeaders = new Headers({
   Authorization: "Bearer " + apiToken,
 });
-var queryString = "?subject_types=vocabulary,kana_vocabulary";
-var apiEndpoint = new Request(
-  "https://api.wanikani.com/v2/" + apiEndpointPath + queryString,
-  {
-    method: "GET",
-    headers: requestHeaders,
-  }
-);
+var queryString =
+  "?subject_types=vocabulary,kana_vocabulary&srs_stages=1,2,3,4,5,6,7,8,9";
+var next_url = "https://api.wanikani.com/v2/" + apiEndpointPath + queryString;
 
-fetch(apiEndpoint)
-  .then((response) => response.json())
-  .then((responseBody) => {
-    console.log(responseBody.data);
+const getAPIResponse = async (endPoint) => {
+  const response = await fetch(endPoint);
+  const responseBody = await response.json();
+
+  return responseBody;
+};
+
+const getCurrentVocab = async () => {
+  const myVocab = [];
+  do {
+    console.log(Object.keys(myVocab).length);
+    let apiEndpoint = new Request(next_url, {
+      method: "GET",
+      headers: requestHeaders,
+    });
+
+    let responseBody = await getAPIResponse(apiEndpoint);
+    next_url = responseBody.pages.next_url;
+    const data = responseBody.data;
+
+    for (let i = 0; i < data.length; i++) {
+      myVocab.push(allVocab[data[i].data.subject_id].characters);
+    }
+  } while (next_url);
+
+  return myVocab;
+};
+
+const exportVocab = async () => {
+  const vocabList = await getCurrentVocab();
+  console.log(vocabList.length);
+  // Convert to JSON
+  const jsonData = JSON.stringify(vocabList);
+  // Write to a file
+  fs.writeFile("myVocab.json", jsonData, (err) => {
+    if (err) throw err;
+    console.log("Data has been saved!");
   });
+};
+
+exportVocab();
